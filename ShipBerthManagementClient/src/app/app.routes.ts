ca40b661274429e607b398b9fb18a1acf746284a
Davor Božanić <d.bozanic@systec.danieli.com>
2025-10-21 10:45:08 +0200

    Small fix

--- ShipBerthManagementClient/src/app/app.routes.ts
+++ ShipBerthManagementClient/src/app/app.routes.ts
@@ -9,10 +9,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-   {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [authGuard],
-  },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: '**', component: NotFoundComponent },
 ];
