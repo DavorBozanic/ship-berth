754898e601d4f9c0833582da229cb87cb2122dce
Davor Božanić <d.bozanic@systec.danieli.com>
2025-10-22 22:05:45 +0200

    Added reservation component

--- ShipBerthManagementClient/src/app/app.routes.ts
+++ ShipBerthManagementClient/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ShipComponent } from './components/ship/ship.component';
 import { BerthComponent } from './components/berth/berth.component';
 import { UserComponent } from './components/user/user.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -28,6 +29,10 @@ export const routes: Routes = [
       {
         path: 'berth',
         component: BerthComponent,
+      },
+      {
+        path: 'reservation',
+        component: ReservationComponent,
       },
     ],
     canActivate: [authGuard]
